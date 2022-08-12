c898c71335f66dd5718cc821e4dd5ecbc52cc321
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-08-12 12:24:06 -0300

    feat: getById na camada de services

--- services/Products.js
+++ services/Products.js
@@ -1,7 +1,14 @@
 const productsModal = require('../models/Products');
+const CustomError = require('../errors/customError');
 
 const productsService = {
   getAll: async () => productsModal.getAll(),
+  getById: async (id) => {
+    const result = await productsModal.getById(id);
+    if (!result) throw new CustomError(404, 'Product not found');
+
+    return result;
+  },
 };
 
 module.exports = productsService;
