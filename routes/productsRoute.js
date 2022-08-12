c9418281e4f522932a0ab44be70bc0aaa0c77015
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-08-12 12:25:12 -0300

    feat: rota /products/:id implementada

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -4,5 +4,6 @@ const productsController = require('../controllers/Products');
 const productsRoute = express.Router();
 
 productsRoute.get('/', productsController.getAll);
+productsRoute.get('/:id', productsController.getById);
 
 module.exports = productsRoute;
