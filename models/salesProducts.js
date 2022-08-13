bedb2d733203e535104612c4e257dd2cfcc99e7a
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-08-12 21:11:14 -0300

    refactor: salesProducts na camada models

--- models/salesProducts.js
+++ models/salesProducts.js
@@ -6,7 +6,8 @@ const salesProducts = {
     const id = await salesModel.create();
     const query = `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
     VALUES (?, ?, ?)`;
-    await Promise.all(body.map(({ productId, quantity }) => connection.execute(query, [id, productId, quantity])));
+    await Promise.all(body.map(({ productId, quantity }) => connection
+      .execute(query, [id, productId, quantity])));
 
     return { id, itemsSold: body };
   },
