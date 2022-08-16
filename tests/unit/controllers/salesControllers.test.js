54997208bd2d0c4dacc56256786f8f2dece17375
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-08-16 16:51:22 -0300

    Finaliza teste sales na camada de controllers

--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -53,4 +53,48 @@ describe('Teste SalesProducts Controllers', () => {
       expect(result.body).to.be.deep.equal(resultService);
     });
   });
+
+  describe('função getById', () => {
+    it('Retorna status 200', async () => {
+      const resultService = [
+        {
+          "date": "2021-09-09T04:54:29.000Z",
+          "productId": 1,
+          "quantity": 2
+        },
+        {
+          "date": "2021-09-09T04:54:54.000Z",
+          "productId": 2,
+          "quantity": 2
+        }
+      ]
+      const params = 1;
+      sinon.stub(salesServices, 'getById').resolves(resultService);
+
+      const result = await testMyController(salesControllers.getById, { params });
+
+      expect(result.status).to.be.equal(200);
+    });
+
+    it('Retorna json com as informações da venda enviada pelo parametro', async () => {
+      const resultService = [
+        {
+          "date": "2021-09-09T04:54:29.000Z",
+          "productId": 1,
+          "quantity": 2
+        },
+        {
+          "date": "2021-09-09T04:54:54.000Z",
+          "productId": 2,
+          "quantity": 2
+        }
+      ]
+      const params = 1;
+      sinon.stub(salesServices, 'getById').resolves(resultService);
+
+      const result = await testMyController(salesControllers.getById, { params });
+
+      expect(result.body).to.be.deep.equal(resultService);
+    });
+  })
 });
